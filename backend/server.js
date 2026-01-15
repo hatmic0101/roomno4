170d574da76f56d4e26daee34eb5c8579e9fcef2
Michał Hatłas <hatmic0101@gmail.com>
2026-01-15 22:56:20 +0100

    zmiany w server.js

--- backend/server.js
+++ backend/server.js
@@ -108,6 +108,8 @@ app.post("/api/signup", async (req, res) => {
 });
 
 // ===============================
-app.listen(process.env.PORT, () => {
-  console.log(`🚀 Backend running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Backend running on port ${PORT}`);
 });
